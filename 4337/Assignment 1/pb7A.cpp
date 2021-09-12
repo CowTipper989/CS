a48a232f9c4b903b418004935adf8294a0c16286
CowTipper989 <wellsluke88@tuta.io>
2021-09-12 17:08:36 -0500

    p14337a1

--- 4337/Assignment 1/pb7A.cpp
+++ 4337/Assignment 1/pb7A.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-    
+    struct Student {
+        string name;
+        int age;
+        float gpa;
+        string gradeLevel;
+    };
+
+    Student stuArray[2];
+    stuArray[0] = {"Sally", 19, 3.78, "freshman"};
+    stuArray[1] = {"Bill", 21, 2, "junior"};
+
+    cout << stuArray[0].name << ' ' << stuArray[0].age << ' ' << stuArray[0].gpa << ' ' << stuArray[0].gradeLevel; 
+    cout << "\n";
+    cout << stuArray[1].name << ' ' << stuArray[1].age << ' ' << stuArray[1].gpa << ' ' << stuArray[1].gradeLevel; 
+    return 0;
 }
---
+++ 4337/Assignment 1/pb7B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string stu1Name = "Sally";
+    int stu1Age = 19;
+    float stu1Gpa = 3.78;
+    string stu1GradeLevel = "freshman";
+    string stu2Name = "Bill";
+    int stu2Age = 21;
+    float stu2Gpa = 2;
+    string stu2GradeLevel = "junior";
+
+    cout << stu1Name << ' ' << stu1Age << ' ' << stu1Gpa << ' ' << stu1GradeLevel; 
+    cout << "\n";
+    cout << stu2Name << ' ' << stu2Age << ' ' << stu2Gpa << ' ' << stu2GradeLevel; 
+    return 0;
+}
